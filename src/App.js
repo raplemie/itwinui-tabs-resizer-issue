2d02998c50443e3a480503e9b320797621d16651
Raphael Lemieux <1904889+raplemie@users.noreply.github.com>
2023-05-31 17:03:57 -0400

    Fix package.json, add visual cue

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import { Button, ButtonGroup } from "@itwin/itwinui-react";
 function App() {
   return (
     <>
+      {/* below div is only a visual helper for the 20% width */}
+      <div
+        style={{
+          position: "absolute",
+          width: "20%",
+          height: "100%",
+          borderRight: "1px solid green",
+        }}
+      ></div>
       <ButtonGroup
         style={{ width: "20%" }}
         overflowButton={() => (
@@ -15,7 +24,8 @@ function App() {
         <Button>Long enough button</Button>
       </ButtonGroup>
       <br />
-      Resize window so the button is more or less than 20% of the window.
+      Resize window so the button is more or less than 20% of the window (green
+      line).
     </>
   );
 }
